6cb1176742bfc448cce728a4a503a784e0e5b6ee
David Phillips <davidp6@uw.edu>
2015-03-18 11:43:53 -0700

    extract_ghdx_jdata in stable state

--- excelhmis/extract_ghdx_jdata.py
+++ excelhmis/extract_ghdx_jdata.py
@@ -19,11 +19,9 @@ Reference an iso3 table to look up country name
 
 # set up python
 # --------------
-import sys
+import sys, platform, os, string, random
 import numpy as np
 import pandas as pd
-import platform
-import os
 
 # inputs
 iso3 = 'ZMB'
@@ -39,7 +37,6 @@ inDir = j + '/DATA/' + iso3 + '/HMIS'
 outDir = j + '/Project/dhis/' + country + '/extracted_data'
 dataFile = outDir + '/data_' + country + '.csv'
 elementIDFile = outDir + '/data_elements.csv'
-categoryIDFile = outDir + '/data_categories.csv'
 orgIDFile = outDir + '/org_units_description.csv'
 
 
@@ -52,7 +49,7 @@ files_xls = [f for f in files if f[-extLength:] == extension and pattern in f]
 nFiles = len(files_xls)
 
 # append files
-print 'Appending ' + str(nFiles) + ' data files containing pattern ' + extension + ' and ' + pattern + ' from ' + inDir
+print 'Appending ' + str(nFiles) + ' data files containing ' + extension + ' and ' + pattern + ' from ' + inDir
 data = pd.DataFrame()
 for f, file in enumerate(files_xls):
     progress = round(f/nFiles,3)*100
@@ -62,8 +59,36 @@ for f, file in enumerate(files_xls):
     data = data.append(df)
     
 # codify data
+print 'Generating unique codes for data elements and organizational units...'
+random.seed(1)
+
+# elements
+elementIDs = pd.DataFrame(columns=['element_ID', 'element_name'])
+elementNames = data.Data.unique()
+elementNames.sort()
+for e, name in enumerate(elementNames):
+    elementIDs = elementIDs.append(pd.Series([str(e), name], index=['element_ID', 'element_name']), ignore_index=True)
+
+# organizational units
+orgUnitIDs = pd.DataFrame(columns=['org_unit_ID', 'org_unit_name'])
+orgUnitNames = data['Organisation unit'].unique()
+orgUnitNames.sort()
+for o, name in enumerate(orgUnitNames):
+    orgUnitIDs = orgUnitIDs.append(pd.Series([str(o), name], index=['org_unit_ID', 'org_unit_name']), ignore_index=True)
+
+# merge ID's, drop names
+data = pd.merge(data, elementIDs, how='left', left_on='Data', right_on='element_name')
+data = pd.merge(data, orgUnitIDs, how='left', left_on='Organisation unit', right_on='org_unit_name')
+data.drop(['Data', 'element_name', 'Organisation unit', 'org_unit_name'], axis=1)
 
 
-# output data
-# -----------
+# output data and codebooks
+# -------------------------
+print 'Saving %s...' % (dataFile)
 data.to_csv(dataFile, encoding='utf-8')
+
+print 'Saving %s...' % (elementIDFile)
+elementIDs.to_csv(elementIDFile, encoding='utf-8')
+
+print 'Saving %s...' % (orgIDFile)
+orgUnitIDs.to_csv(orgIDFile, encoding='utf-8')
