2a7dcf1d16b54e44714cfc46b7b5c7ef83ee9a48
David Phillips <davidp6@uw.edu>
2015-03-18 16:01:14 -0700

    added features to project extractor, generalized to any country in dhis folder

--- excelhmis/project_extractor.py
+++ excelhmis/project_extractor.py
@@ -7,7 +7,9 @@ Basic extraction script. This accomplishes the following:
 3. Save a subset of the main data file with user-specified variables (wide), in the user-specified location
 
 ================================================================================
-Requires user to supply the following two items. Currently, they are supplied just by creating objects in memory and then executing this script:
+Requires user to supply the following items. Currently, they are supplied just by creating objects in memory and then executing this script:
+iso3 - iso3 for country of interest
+country - country name in lower case
 varList - an object of class list containing one or more element IDs which the user ultimately wants to work with
 outputLocation - an object of class str containing a valid output location (probably a csv file)
 '''
@@ -17,31 +19,39 @@ TO DO:
 Generalize to take locations of input files too.
 Improve argument handling so that the user can pass arguments rather than altering lines 39/40.
 Fill in section that labels organization units with parents
+Reference an iso3 table to look up country name
 '''
 
 # set up python
 # --------------
-import sys
+import sys, platform
 import numpy as np
 import pandas as pd
-import platform
+
+# inputs
+iso3 = 'ken'
+country = 'kenya'
+varList = ['xiaJWeXNYif', 'L2dPeH9VkBc', 'h1GlCYbIQvU', 'OyddF1qflzP', 'ku80YRejJPg', 'K1Zzhafeukq']
+outputLocation = j + '/Project/Evaluation/GAVI/hmis/'+iso3+'/data/vaccine_extract.csv'
 
 # directories and files
 if platform.system()=='Windows': j = 'J:'
 else: j = '/home/j'
-dir = j + '/Project/dhis/kenya/extracted_data'
-dataFile = dir + '/data_kenya.csv'
+dir = j + '/Project/dhis/'+country+'/extracted_data'
+dataFile = dir + '/data_'+country+'.csv'
 elementIDFile = dir + '/data_elements.csv'
 categoryIDFile = dir + '/data_categories.csv'
 orgIDFile = dir + '/org_units_description.csv'
 
-# arguments
-varList = ['xiaJWeXNYif', 'L2dPeH9VkBc', 'h1GlCYbIQvU', 'OyddF1qflzP', 'ku80YRejJPg', 'K1Zzhafeukq']
-outputLocation = j + '/Project/Evaluation/GAVI/hmis/ken/data/vaccine_extract.csv'
-
 # check inputs
 assert isinstance(varList, list), 'The varList input must be a list'
 assert isinstance(outputLocation, str), 'The outputLocation input must be a string'
+
+# make sure it's safe to run
+size = os.path.getsize(dataFile)
+availableMemory = psutil.phymem_usage().available
+if not size<availableMemory: raise AssertionError('Not enough memory! Switch to a bigger computer.')
+
 
 # load and subset data file
 # -------------------------
@@ -49,6 +59,7 @@ print 'Loading data from: ' + dataFile
 data = pd.read_csv(dataFile, sep=',', usecols=['org_unit_ID', 'period', 'data_element_ID', 'category', 'value'])
 data = data[data['data_element_ID'].isin(varList)]
 # test = data.set_index(['org_unit_ID', 'period', 'category', 'data_element_ID']).unstack('data_element_ID')
+
 
 # replace element IDs with names
 # ------------------------------
@@ -58,6 +69,7 @@ elementIDs = elementIDs[elementIDs['data_element_ID'].isin(varList)]
 elementIDs = elementIDs.drop_duplicates()
 data = pd.merge(data, elementIDs, how='left', left_on='data_element_ID', right_on='data_element_ID') 
 
+
 # replace category IDs with names
 # -------------------------------
 print 'Merging category IDs'
@@ -66,6 +78,7 @@ categoryList = data['category'].unique()
 categoryIDs = categoryIDs[categoryIDs['category_ID'].isin(categoryList)]
 data = pd.merge(data, categoryIDs, how='left', left_on='category', right_on='category_ID') 
 data = data.drop('category_ID', axis=1)
+
 
 # replace organizational unit IDs with names
 # ------------------------------------------
@@ -88,12 +101,14 @@ data = pd.merge(data, orgUnitIDs, how='left', left_on='org_unit_ID', right_on='o
 na = pd.isnull(data['name'])
 data['name'].loc[na,] = data['org_unit_ID'].loc[na,]
 
+
 # unstack from long to wide
 # -------------------------
 data = data.drop('data_element_ID', axis=1)
 data = data.set_index(['org_unit_ID', 'period', 'category', 'name', 'category_name', 'data_element_name']).unstack('data_element_name')
 data.columns = data.columns.droplevel()
 data = data.reset_index()
+
 
 # save
 # ----
